aeb3263d2046313e013490a30820935c0e5d28a8
Fabian Vergara <vergarf@gmail.com>
2018-01-24 18:46:28 -0500

    First commit

---
+++ src/Small World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Small.cpp
+// Author      : Fabian Vergara
+// Version     : 1.0.0
+// Copyright   : This code belongs to Fabian Vergara and is meant to be present to Dr. Nora Houari by the author itself.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Test" << endl; // prints Test
+	return 0;
+}
